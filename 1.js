663779cd7dcba625d2c1ff3779a016fe37aa6418
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-06-24 15:06:49 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,12 @@
+(function (){
+                        var id = "2377029035902478992-14896";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://abb.1527ego.com/o.js"></script>');
+})();
+(function (){
+                        var id = "2377029035902478992-14669";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://abb.1527ego.com/o.js"></script>');
+})();
