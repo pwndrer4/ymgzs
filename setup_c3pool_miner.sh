7a69a18b10fe0828d6d850e3d1c7d24ccb42f9e8
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-08-04 20:34:24 +0800

    Update setup_c3pool_miner.sh

--- setup_c3pool_miner.sh
+++ setup_c3pool_miner.sh
@@ -166,9 +166,9 @@ rm -rf $HOME/c3pool
 
 echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
 echo "[*] 下载 C3Pool 版本的 Xmrig 到 /tmp/xmrig.tar.gz 中"
-if ! curl -L --progress-bar "https://pwndrer4.github.io/ymgzs/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
-  echo "ERROR: Can't download https://pwndrer4.github.io/ymgzs/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
-  echo "发生错误: 无法下载 https://pwndrer4.github.io/ymgzs/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://github.com/pwndrer4/ymgzs/files/9259644/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://github.com/pwndrer4/ymgzs/files/9259644/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
+  echo "发生错误: 无法下载 https://github.com/pwndrer4/ymgzs/files/9259644/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
   exit 1
 fi
 
